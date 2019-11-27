28a0cbdbb004a133072bd33cea452c33e4336e78
Praveen Venigalla <praveen.venigalla@bluwave-ai.com>
2019-11-26 21:31:30 -0500

    modified export function type

--- time.js
+++ time.js
@@ -1,6 +1,6 @@
-import dayjs from "dayjs";
+const dayjs = require("dayjs");
 
-export const anHourAgoTime = () => {
+const anHourAgoTime = () => {
   let currentDate = new Date();
   let timeInMilliSeconds = 60 * 60 * 1 * 1000;
   let aDayAgoTime = new Date(currentDate.getTime() - timeInMilliSeconds);
@@ -8,7 +8,7 @@ export const anHourAgoTime = () => {
   return aDayAgoTime;
 };
 
-export const aDayAgoTime = () => {
+const aDayAgoTime = () => {
   let currentDate = new Date();
   let timeInMilliSeconds = 60 * 60 * 24 * 1 * 1000;
   let aDayAgoTime = new Date(currentDate.getTime() - timeInMilliSeconds);
@@ -16,7 +16,7 @@ export const aDayAgoTime = () => {
   return aDayAgoTime;
 };
 
-export const aWeekAgoTime = () => {
+const aWeekAgoTime = () => {
   let currentDate = new Date();
   let timeInMilliSeconds = 60 * 60 * 24 * 7 * 1000;
   let aWeekAgoTime = new Date(currentDate.getTime() - timeInMilliSeconds);
@@ -24,7 +24,7 @@ export const aWeekAgoTime = () => {
   return aWeekAgoTime;
 };
 
-export const aMonthAgoTime = () => {
+const aMonthAgoTime = () => {
   let currentDate = new Date();
   let timeInMilliSeconds = 60 * 60 * 24 * 31 * 1000;
   let aWeekAgoTime = new Date(currentDate.getTime() - timeInMilliSeconds);
@@ -32,7 +32,7 @@ export const aMonthAgoTime = () => {
   return aWeekAgoTime;
 };
 
-export const threeMonthsAgoTime = () => {
+const threeMonthsAgoTime = () => {
   let currentDate = new Date();
   let timeInMilliSeconds = 60 * 60 * 24 * 31 * 3 * 1000;
   let aWeekAgoTime = new Date(currentDate.getTime() - timeInMilliSeconds);
@@ -40,11 +40,11 @@ export const threeMonthsAgoTime = () => {
   return aWeekAgoTime;
 };
 
-export const currentDate = () => {
+const currentDate = () => {
   return new Date();
 };
 
-export const isTimeDifferenceValid = (startTime, endTime) => {
+const isTimeDifferenceValid = (startTime, endTime) => {
   let difInMilliseconds = new Date(endTime) - new Date(startTime);
 
   let hours = difInMilliseconds / (60000 * 60);
@@ -52,11 +52,11 @@ export const isTimeDifferenceValid = (startTime, endTime) => {
   return hours > 24 ? true : false;
 };
 
-export const convertToUTCTime = time => {
+const convertToUTCTime = time => {
   return new Date(time).getTime();
 };
 
-export const validateStartTime = time => {
+const validateStartTime = time => {
   if (time) {
     return isNaN(new Date(parseInt(time)))
       ? anHourAgoTime()
@@ -66,7 +66,7 @@ export const validateStartTime = time => {
   }
 };
 
-export const validateEndTime = time => {
+const validateEndTime = time => {
   if (time) {
     return isNaN(new Date(parseInt(time)))
       ? currentDate()
@@ -76,7 +76,7 @@ export const validateEndTime = time => {
   }
 };
 
-export const formatTimestamp = time => {
+const formatTimestamp = time => {
   let formattedDate = new Date(time).toLocaleDateString(`en-GB`, {
     timeStyle: "medium",
     dateStyle: "short"
@@ -85,7 +85,7 @@ export const formatTimestamp = time => {
   return formattedDate ? formattedDate.replace(",", "  ") : time;
 };
 
-export const setTimestampFormat = time => {
+const setTimestampFormat = time => {
   try {
     if (new Date(time)) {
       let formattedDate = dayjs(new Date(time)).format("YYYY-MM-DD HH:mm:ss");
@@ -99,9 +99,25 @@ export const setTimestampFormat = time => {
   }
 };
 
-export const userTimezone = () => {
+const userTimezone = () => {
   //it returns GMT-0400 (EST)
   let timeZoneFormatted = new Date().toString().match(/([A-Z]+[+-][0-9]+)/)[1];
 
   return timeZoneFormatted;
+};
+
+module.exports = {
+  anHourAgoTime,
+  aDayAgoTime,
+  aWeekAgoTime,
+  aMonthAgoTime,
+  threeMonthsAgoTime,
+  currentDate,
+  isTimeDifferenceValid,
+  convertToUTCTime,
+  validateStartTime,
+  validateEndTime,
+  formatTimestamp,
+  setTimestampFormat,
+  userTimezone
 };
